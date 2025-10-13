aced23caf4a6f59b4c9699c8eb33bb7a9c2be45f
renellerobief16-source <renellerobieF16@gmail.com>
2025-10-13 10:29:08 +0800

    Version 1 - My Base Script

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickButton")?.addEventListener("click", function () {
+  const message = document.getElementById("message");
+  message.textContent = "🎉 You clicked the button! Great job!";
+});
